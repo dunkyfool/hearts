e06ac9be6465577c025069be6a9c2cdf65cca4ba
Jackie <jackie@ghtinc.com>
2018-08-14 21:02:39 +0800

    update RL

--- code/RL.py
+++ code/RL.py
@@ -38,18 +38,124 @@ class DQN:
                 b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
                 self.q_next = tf.matmul(l1, w2) + b2
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        n_actions,
+        n_features,
+        learning_rate=0.01,
+        reward_decay=0.9,
+        e_greedy=0.9,
+        replace_target_iter=300,
+        memory_size=500,
+        batch_size=32,
+        e_greedy_increment=None,
+        output_graph=False,
+    ):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon_max = e_greedy
+        self.replace_target_iter = replace_target_iter  # change target_net's step
+        self.memory_size = memory_size  # maximum of replay memory
+        self.batch_size = batch_size
+        self.epsilon_increment = e_greedy_increment # epsilon's increment
+        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max # flag: explore mode
+
+        #  as counter for changing target_net's weighting
+        self.learn_step_counter = 0
+
+        # initialize memory [s, a, r, s_] to zero
+        self.memory = np.zeros((self.memory_size, n_features*2+2))
+
+        # create [target_net, evaluate_net]
+        self._build_net()
+
+        # change target net's weightinh
+        t_params = tf.get_collection('target_net_params')  # extract target_net's weighting
+        e_params = tf.get_collection('eval_net_params')    # extract eval_net's weighting
+        self.replace_target_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)] # update target_net's weighting
+
+        self.sess = tf.Session()
+
+        # output tensorboard's document
+        if output_graph:
+            # $ tensorboard --logdir=logs
+            tf.summary.FileWriter("logs/", self.sess.graph)
+
+        self.sess.run(tf.global_variables_initializer())
+        self.cost_his = []  # record cost
 
     def store_transition(self, s, a, r, s_):
-        pass
+        if not hasattr(self, 'memory_counter'):
+            self.memory_counter = 0
+
+        # record [s, a, r, s_] 
+        transition = np.hstack((s, [a, r], s_))
+
+        # if the memory reach the limit, the new one will overwrite the old one
+        index = self.memory_counter % self.memory_size
+        self.memory[index, :] = transition
+
+        self.memory_counter += 1
 
     def choose_action(self, observation):
         pass
+        # make observation consistent
+        observation = observation[np.newaxis, :]
+
+        if np.random.uniform() < self.epsilon:
+            # get the max action from the eval_net 
+            actions_value = self.sess.run(self.q_eval, feed_dict={self.s: observation})
+            action = np.argmax(actions_value)
+        else:
+            action = np.random.randint(0, self.n_actions)   # random pick
+        return action
 
     def learn(self):
         pass
+        # check target_net's weighting
+        if self.learn_step_counter % self.replace_target_iter == 0:
+            self.sess.run(self.replace_target_op)
+            print('\ntarget_params_replaced\n')
+
+        # get memory 
+        if self.memory_counter > self.memory_size:
+            sample_index = np.random.choice(self.memory_size, size=self.batch_size)
+        else:
+            sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
+        batch_memory = self.memory[sample_index, :]
+
+        # retrieve q_next from target_net and q_eval from eval_net
+        q_next, q_eval = self.sess.run(
+            [self.q_next, self.q_eval],
+            feed_dict={
+                self.s_: batch_memory[:, -self.n_features:],
+                self.s: batch_memory[:, :self.n_features]
+            })
+
+        q_target = q_eval.copy()
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+        eval_act_index = batch_memory[:, self.n_features].astype(int)
+        reward = batch_memory[:, self.n_features + 1]
+
+        q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
+
+
+        # train eval_net
+        _, self.cost = self.sess.run([self._train_op, self.loss],
+                                     feed_dict={self.s: batch_memory[:, :self.n_features],
+                                                self.q_target: q_target})
+        self.cost_his.append(self.cost) # record cost
+
+        # increase epsilon and decrease random 
+        self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
+        self.learn_step_counter += 1
 
     def plot_cost(self):
-        pass
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(len(self.cost_his)), self.cost_his)
+        plt.ylabel('Cost')
+        plt.xlabel('training steps')
+        plt.show()
 
